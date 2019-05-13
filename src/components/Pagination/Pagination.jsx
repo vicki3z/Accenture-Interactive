0c109f06ce2fbb3cf3b2e101fb89362984719187
victoriapong <victoria.pong@gmail.com>
2019-05-13 17:39:40 +0700

    Update: Pagination - get people button to connect with api call logic

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -3,9 +3,13 @@ import React from "react";
 /* styling */
 import "./Pagination.css";
 
-const Pagination = () => (
+/**
+ * Pagination - Pagination component for all the buttons
+ * @param {function} props.handleClick - handle click function
+ */
+const Pagination = ({ handleClick }) => (
   <div className="pagination-wrapper">
-    <button>Get the people</button>
+    <button onClick={handleClick}>Get the people</button>
   </div>
 );
 
